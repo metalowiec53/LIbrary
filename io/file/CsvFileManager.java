80a4e6daac3dcc90ca0cb4688a8984b30db3c526
metalowiec53 <78949381+metalowiec53@users.noreply.github.com>
2021-03-22 16:13:19 +0100

    Add files via upload

---
+++ io/file/CsvFileManager.java
@@ -0,0 +1,81 @@
+package pl.MB.library.io.file;
+import pl.MB.library.exception.DataExportExceptio;
+import pl.MB.library.exception.DataImportException;
+import pl.MB.library.exception.InvalidDataException;
+import pl.MB.library.model.Library;
+import pl.MB.library.model.Publication;
+import pl.MB.library.model.Book;
+import pl.MB.library.model.Magazine;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class CsvFileManager implements FileManager {
+    private static final String FILE_NAME = "Library.csv";
+
+    @Override
+    public void exportData(Library library) {
+        Publication[] publications = library.getPublications();
+        try (FileWriter fileWriter = new FileWriter(FILE_NAME);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            for (Publication publication : publications) {
+                bufferedWriter.write(publication.toCsv());
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            throw new DataExportExceptio("Błąd zapisu danych do pliku " + FILE_NAME);
+        }
+    }
+
+    @Override
+    public Library importData() {
+        Library library = new Library();
+        try (Scanner fileReader = new Scanner(new File(FILE_NAME))) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                Publication publication = createObjectFromString(line);
+                library.addPublication(publication);
+            }
+        } catch (FileNotFoundException e) {
+            throw new DataImportException("Brak pliku " + FILE_NAME);
+        }
+        return library;
+    }
+
+    private Publication createObjectFromString(String csvText) {
+        String[] split = csvText.split(";");
+        String type = split[0];
+        if(Book.TYPE.equals(type)) {
+            return createBook(split);
+        } else if(Magazine.TYPE.equals(type)) {
+            return createMagazine(split);
+        }
+        throw new InvalidDataException("Nieznany typ publikacji: " + type);
+    }
+
+    private Book createBook(String[] data) {
+        String title = data[1];
+        String publisher = data[2];
+        int year = Integer.valueOf(data[3]);
+        String author = data[4];
+        int pages = Integer.valueOf(data[5]);
+        String isbn = data[6];
+        return new Book(title, author, year, pages, publisher, isbn);
+    }
+
+    private Magazine createMagazine(String[] data) {
+        String title = data[1];
+        String publisher = data[2];
+        int year = Integer.valueOf(data[3]);
+        int month = Integer.valueOf(data[4]);
+        int day = Integer.valueOf(data[5]);
+        String language = data[6];
+        return new Magazine(title, publisher, language, year, month, day);
+    }
+
+}
